22e6ed105789605102feb6939b5bab222deb2cf4
Elian ORIOU <elian.oriou@4sh.fr>
2014-07-25 00:04:47 +0200

    Much more simple structure on server side

--- srv/src/main/java/vin/io/resources/WineBottlesResource.java
+++ srv/src/main/java/vin/io/resources/WineBottlesResource.java
@@ -28,13 +28,10 @@ public class WineBottlesResource {
     @POST("/bottles")
     public Iterable<WineBottle> addBottle(WineBottle bottle) {
 
-        // The mongo collection
         MongoCollection bottles = this.bottles.get();
 
-        // Add new bottle
         bottles.insert(bottle);
 
-        // Returns new list of bottles
         return bottles.find().as(WineBottle.class);
     }
 
