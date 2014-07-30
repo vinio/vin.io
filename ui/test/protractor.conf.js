6bf9a16d784a727a0eaabe07e18148a010e8a366
Elian ORIOU <elian.oriou@4sh.fr>
2014-07-30 23:11:16 +0200

    Add protractor deps

---
+++ ui/test/protractor.conf.js
@@ -0,0 +1,23 @@
+exports.config = {
+
+    // The address of a running selenium server.
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
+    // App BaseURL
+    baseUrl: 'http://localhost:8080/#/',
+
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    // list of specs
+    specs: [
+        'test/e2e/**/*.js'
+    ],
+
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true
+    }
+};
