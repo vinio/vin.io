f3006dd6190545060994eda12c498f26ed4c01b6
Elian ORIOU <elian.oriou@4sh.fr>
2014-07-31 00:26:50 +0200

    Update sample test

--- ui/test/e2e/master_details_e2e.js
+++ ui/test/e2e/master_details_e2e.js
@@ -1,17 +1,29 @@
-
 var loadUrl = function (url) {
-    browser.get(url);
+    browser.get(browser.baseUrl + url);
 };
 
 describe('master/details e2e test', function () {
 
-    var ptor = protractor.getInstance();
+    var domainNames = [
+        { domain: 'Chateau Petrus', vintage: '2008', description: 'Très bon !'},
+        { domain: 'Chateau Margaux', vintage: '2005', description: 'Très très bon !'}
+    ];
 
     it('should load bottles properly', function () {
 
         loadUrl('/bottles');
 
-        element(by.repeater('bottle in bottles').row(0)).$('#copyCalc').click()
+        var rows = element.all(by.repeater('bottle in bottles'));
+
+        expect(rows.count()).toEqual(domainNames.length);
+
+        for (var i = 0; i < domainNames.length; i++) {
+            var expects = domainNames[i];
+            var row = rows.get(i);
+            expect(row.element(by.binding('bottle.domain')).getText()).toEqual(expects.domain);
+            expect(row.element(by.binding('bottle.vintage')).getText()).toEqual(expects.vintage);
+            expect(row.element(by.binding('bottle.description')).getText()).toEqual(expects.description);
+        }
     });
 
 });
