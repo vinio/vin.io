1dc0fb5791b955e42cc53a83104f95a5848e9466
Xavier Hanin <xavier.hanin@4sh.fr>
2014-10-17 11:45:24 +0200

    #4 client

--- ui/app/scripts/controllers/BottlesCtrl.js
+++ ui/app/scripts/controllers/BottlesCtrl.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('vin.io')
-    .controller('BottlesCtrl', function ($scope, Bottle) {
+    .controller('BottlesCtrl', function ($scope, Bottle, Notification) {
 
         $scope.bottles = Bottle.query();
+
+        $scope.showDetail = function(bottle) {
+            $scope.selection = bottle;
+        };
+
+        $scope.edit = function(bottle) {
+            Bottle.save(bottle).$promise
+                .then(function () {
+                    Notification.notify.success('OK');
+                })
+                .catch(function () {
+                    Notification.notify.error('OK');
+                });
+        };
 
     });
